f5ac89cd2bacaf64cfe086fb613c75a2bb6d8a8d
AJ Picard <Contact@saucedesigns.io>
2021-03-16 08:46:15 -0400

    Initial Commit

---
+++ Shared/ParkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingAppApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ParkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
